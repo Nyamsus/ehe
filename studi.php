cfa505361b7eaa7e752cabe97e64002af964842f
Nyamsus <114452368+Nyamsus@users.noreply.github.com>
2022-09-27 08:54:14 +0800

    Add files via upload

---
+++ studi.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="saile.css">
+    <title>Studi Kasus</title>
+</head>
+
+<body>
+    <div class="menu">
+        <ul id="navigasi">
+            <li><a href="#">Coding</a></li>
+            <li><a id="tent" href="#">Tentang</a>
+            <li><a href="#">Pelajaran</a>
+            <li><a href="#">Hubungi Kami</a>
+        </ul>
+    </div>
+    <br>
+    <br>
+    <h1 id="hello">HELLO WORLD<span id="spa">.</span></h1>
+    <h2 id="ayo">Ayo belajar coding</h2>
+    <br>
+    <p id="teng"><b>Pelajaran</b></p>
+    <hr id="hr">
+    <br>
+    <ul>
+        <img id="img" src="HTML.png">
+        <img id="img" src="CSS.png">
+        <img id="img" src="JS.png">
+        <img id="img" src="PHP.png">
+    </ul>
+    <ul>
+        <li id="bawah">HTML</li>
+        <li id="bawah2">CSS</li>
+        <li id="bawah3">JS</li>
+        <li id="bawah4">PHP</li>
+    </ul>
+    <br>
+    <br>
+    <p id="teng"><b>Hubungi Kami</b></p>
+    <hr id="hr">
+    <form action="">
+        <br>
+        <label id="teng">Nama<span id="span">*</span></label>
+        <br>
+        <input id="atas" type="text" required>
+        <br>
+        <label id="teng">Email<span id="span">*</span></label>
+        <br>
+        <input id="atas" type="text" required>
+        <br>
+        <label id="teng">Pesan<span id="span">*</span></label>
+        <br>
+        <input id="atas" type="text" required>
+        <br>
+        <label id="teng"><span id="span">*</span>Bidang Wajib Diisi</label>
+        <br>
+        <button type="submit" id="sumbit">Kirim</button>
+    </form>
+
+
+</body>
+<footer>
+    <div id="div">
+        <br>
+        <h1 style="color: white; margin-left: 30px;;">Coding</h1>
+        <p id="p">Tentang</p>
+        <p id="p">Pelajaran</p>
+        <p id="p">Hubungi Kami</p>
+        <br>
+    </div>
+</footer>
+
+</html>
